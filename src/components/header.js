582dce7165118f8636d7723c23b9d5699d961889
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-03-11 13:05:05 -0500

    Created header

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,29 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  // Creating elements
+  const headerDiv = document.createElement("div");
+  const dateElement = document.createElement("span");
+  const titleElement = document.createElement("h1");
+  const tempElement = document.createElement("span");
+
+  // Adding classes
+  headerDiv.classList.add("header");
+  dateElement.classList.add("date");
+  tempElement.classList.add("temp");
+
+  // Assigning Values
+  dateElement.textContent = date;
+  titleElement.textContent = title;
+  tempElement.textContent = temp;
+
+  // Connecting them together
+  headerDiv.appendChild(dateElement);
+  headerDiv.appendChild(titleElement);
+  headerDiv.appendChild(tempElement);
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +43,18 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const header = Header("Lorem", "Ifsum", "Dimsum");
+
+  const wrapper = document.createElement("div");
+  wrapper.classList.add(selector);
+  wrapper.appendChild(header)
+
+
+  document.querySelector("body").appendChild(wrapper);
+
+
+  // console.log(header);
+  // return header;
 }
 
 export { Header, headerAppender }
